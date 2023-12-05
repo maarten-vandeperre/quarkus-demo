f9fc79dc1cd5554b4bb5bad9ef16d81eab780d3d
maartenvandeperre <maarten.vandeperre@viae.solutions>
2023-12-05 10:00:59 +0100

    add initial_setup.sh script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # quarkus-demo
+
+Project based upon https://redhat-developer-demos.github.io/quarkus-tutorial/quarkus-tutorial/index.html
---
+++ initial_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl -s "https://get.sdkman.io" | bash ;
+source "$HOME/.sdkman/bin/sdkman-init.sh" . ;
+sdk install java ;
+sdk install maven ;
+sdk install quarkus ;
+sdk install java 17.0.8-graalce ;
+sdk install jbang ;
