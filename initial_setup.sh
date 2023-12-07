f836d8c620df2d8e3f8d92cca022eabb8eb03715
maartenvandeperre <maarten.vandeperre@viae.solutions>
2023-12-07 09:04:22 +0100

    increment versions

--- initial_setup.sh
+++ initial_setup.sh
@@ -2,10 +2,10 @@
 
 curl -s "https://get.sdkman.io" | bash ;
 source "$HOME/.sdkman/bin/sdkman-init.sh" . ;
-sdk install java ;
-sdk install maven ;
-sdk install quarkus ;
-sdk install java 17.0.8-graalce ;
-sdk install jbang ;
+sdk install java 21.0.1-tem ;
+sdk install maven 3.9.6 ;
+sdk install quarkus 3.6.0 ;
+sdk install java 21.0.1-graalce ;
+sdk install jbang 0.113.0 ;
 source "$HOME/.sdkman/bin/sdkman-init.sh" . ;
 echo "DONE" ;
